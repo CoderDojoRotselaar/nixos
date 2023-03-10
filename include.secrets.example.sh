be3a08c713a726c75beb7c6517dc13ee0d3f4e07
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2023-03-10 13:52:31 +0100

    Better check for wifi
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- include.secrets.example.sh
+++ include.secrets.example.sh
@@ -6,14 +6,7 @@ function _run_before_network() {
   # WIFI_SSID="my-ssid"
   # WIFI_PASSWORD="my-password"
 
-  # wpa_cli -g "${WIFI_DEVICE}" remove_network 0 &>/dev/null
-  # wpa_cli -g "${WIFI_DEVICE}" add_network
-  # wpa_cli -g "${WIFI_DEVICE}" set_network 0 ssid "\"${WIFI_SSID}\""
-  # wpa_cli -g "${WIFI_DEVICE}" set_network 0 psk "\"${WIFI_PASSWORD}\""
-
-  # wpa_cli -g "${WIFI_DEVICE}" enable_network 0
-
-  # _test_network 30
+  # _create_network "${WIFI_SSID}" "${WIFI_PASSWORD}"
   :
 }
 function _run_before_install() {
--- install.sh
+++ install.sh
@@ -69,16 +69,29 @@ function _connect_wifi() {
     return
   fi
 
+  read -s -p "Please enter your WIFI password: " -r WIFI_PASSWORD
+
+  _create_network "${WIFI_SSID}" "${WIFI_PASSWORD}"
+}
+
+function _create_network() {
+  if [[ -z "${WIFI_DEVICE}" ]]; then
+    return
+  fi
+
+  if [[ -z "${WIFI_SSID}" ]]; then
+    return
+  fi
+
   wpa_cli -g "${WIFI_DEVICE}" remove_network 0 &>/dev/null
   wpa_cli -g "${WIFI_DEVICE}" add_network
   wpa_cli -g "${WIFI_DEVICE}" set_network 0 ssid "\"${WIFI_SSID}\""
 
-  read -s -p "Please enter your WIFI password: " -r WIFI_PASSWORD
   wpa_cli -g "${WIFI_DEVICE}" set_network 0 psk "\"${WIFI_PASSWORD}\""
 
   wpa_cli -g "${WIFI_DEVICE}" enable_network 0
 
-  _network 30
+  _test_network 30
 }
 
 function _format() {
@@ -116,7 +129,7 @@ function _clone() {
   cd /mnt/etc/nixos
 
   DISK_NAME=$(basename "${MAIN_DISK}")
-  ln -s disk.nix "disk_${DISK_NAME}.nix"
+  ln -s "disk_${DISK_NAME}.nix" disk.nix
 }
 
 function _generate_config() {
