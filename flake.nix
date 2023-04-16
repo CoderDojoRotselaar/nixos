ebc3c0e49ff30cb6ddb9c44441d280447a27b1a5
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2023-04-16 22:19:50 +0200

    Set all hostnames
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- flake.nix
+++ flake.nix
@@ -21,9 +21,7 @@
         ./virtual_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
-        ({pkgs, ...}: {
-          networking.hostName = "cdjvm-jovdg-1";
-        })
+        ({pkgs, ...}: {networking.hostName = "cdjvm-jovdg-1";})
       ];
     };
     nixosConfigurations."cdj-rots-114" = nixpkgs.lib.nixosSystem {
@@ -33,6 +31,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-114";})
       ];
     };
     nixosConfigurations."cdj-rots-110" = nixpkgs.lib.nixosSystem {
@@ -42,6 +41,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-110";})
       ];
     };
     nixosConfigurations."cdj-rots-102" = nixpkgs.lib.nixosSystem {
@@ -51,6 +51,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-102";})
       ];
     };
     nixosConfigurations."cdj-rots-112" = nixpkgs.lib.nixosSystem {
@@ -60,6 +61,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-112";})
       ];
     };
     nixosConfigurations."cdj-rots-109" = nixpkgs.lib.nixosSystem {
@@ -69,6 +71,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-109";})
       ];
     };
     nixosConfigurations."cdj-rots-106" = nixpkgs.lib.nixosSystem {
@@ -78,6 +81,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-106";})
       ];
     };
     nixosConfigurations."cdj-rots-104" = nixpkgs.lib.nixosSystem {
@@ -87,6 +91,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-104";})
       ];
     };
     nixosConfigurations."cdj-rots-101" = nixpkgs.lib.nixosSystem {
@@ -96,6 +101,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-101";})
       ];
     };
     nixosConfigurations."cdj-rots-111" = nixpkgs.lib.nixosSystem {
@@ -105,6 +111,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-111";})
       ];
     };
     nixosConfigurations."cdj-rots-113" = nixpkgs.lib.nixosSystem {
@@ -114,6 +121,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-113";})
       ];
     };
     nixosConfigurations."cdj-rots-105" = nixpkgs.lib.nixosSystem {
@@ -123,6 +131,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-105";})
       ];
     };
     nixosConfigurations."cdj-rots-108" = nixpkgs.lib.nixosSystem {
@@ -132,6 +141,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-108";})
       ];
     };
     nixosConfigurations."cdj-rots-100" = nixpkgs.lib.nixosSystem {
@@ -141,6 +151,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-100";})
       ];
     };
     nixosConfigurations."cdj-rots-103" = nixpkgs.lib.nixosSystem {
@@ -150,6 +161,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-103";})
       ];
     };
     nixosConfigurations."cdj-rots-107" = nixpkgs.lib.nixosSystem {
@@ -159,6 +171,7 @@
         ./physical_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "cdj-rots-107";})
       ];
     };
     nixosConfigurations.coderdojo = nixpkgs.lib.nixosSystem {
@@ -168,6 +181,7 @@
         ./virtual_hw.nix
         ./hardware.nix
         home-manager.nixosModules.home-manager
+        ({pkgs, ...}: {networking.hostName = "coderdojo";})
       ];
     };
   };
